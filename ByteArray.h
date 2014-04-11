620315fe20c2cfe471f17221194e590c81a06013
Thulio Assis <f0rmiga@hotmail.com>
2014-04-11 14:55:29 -0300

    first commit

---
+++ ByteArray.h
@@ -0,0 +1,157 @@
+//============================================================================
+// Name        : ByteArray.h
+// Author      : Thulio Ferraz Assis
+// Contact     : contato@thulioassis.com.br
+// Copyright   : Thulio Ferraz Assis all rights reserved
+//============================================================================
+
+#include <vector>
+
+using namespace std;
+
+class ByteArray {
+public:
+	/* Initialize empty ByteArray with predefined size */
+	ByteArray(unsigned int size_) {
+		size = size_;
+		bytesAvailable_ = size;
+		vector_.reserve(size);
+	}
+	/* Initialize ByteArray using existing vector of char */
+	ByteArray(vector<char> newvector_) {
+		size = newvector_.size();
+		bytesAvailable_ = size;
+		vector_.reserve(size);
+		vector_.assign(newvector_.begin(), newvector_.end());
+	}
+
+	/* Set the position to write or read */
+	void setPosition(unsigned int position_) {
+		position = position_;
+		bytesAvailable_ = size - position;
+	}
+
+	/* Returns actual position to write or read */
+	unsigned int getPosition() {
+		return position;
+	}
+
+	/* Number of bytes available to read */
+	unsigned int bytesAvailable() {
+		return bytesAvailable_;
+	}
+
+	/* Returns related to the vector */
+	vector<char> result() { return vector_; }
+	unsigned int getSize() { return size; }
+
+	/* Write functions */
+	/* returns true if wrote, otherwise, returns false */
+	bool writeByte(char byte_) {
+		if (position > size - 1) {
+			return false;
+		}
+		vector_[position] = byte_;
+		position++;
+		bytesAvailable_ = size - position;
+		return true;
+	}
+
+	bool writeShort(short short_) {
+		if (bytesAvailable_ < 2) {
+			return false;
+		}
+		return writeByte(short_ & 0xFF) && writeByte(short_ >> 8);
+	}
+
+	bool writeInt(int int_) {
+		if (bytesAvailable_ < 4) {
+			return false;
+		}
+		return writeByte(int_ & 0xFF) && writeByte((int_ >> 8) & 0xFF) && writeByte((int_ >> 16) & 0xFF) && writeByte(int_ >> 24);
+	}
+
+	/* Read functions */
+	/* Throws exception if try to read out of bounds */
+	char readByte() {
+		char byte_ = vector_[position];
+		if (position >= size) {
+			throw 1;
+		}
+		position++;
+		bytesAvailable_ = size - position;
+		return byte_;
+	}
+	char readByte(unsigned int offset) {
+		if (offset < size) {
+			position = offset;
+		}
+		return readByte();
+	}
+
+	unsigned char readUnsignedByte() {
+		return readByte();
+	}
+	unsigned char readUnsignedByte(unsigned int offset) {
+		return readByte(offset);
+	}
+
+	short readShort() {
+		short short_ = ((int)(unsigned char)vector_[position + 1] << 8) | (unsigned char)vector_[position];
+		if (position >= size -1) {
+			throw 1;
+		}
+		position += 2;
+		bytesAvailable_ = size - position;
+		return short_;
+	}
+	short readShort(unsigned int offset) {
+		if (offset < size - 1) {
+			position = offset;
+		}
+		return readShort();
+	}
+
+	unsigned short readUnsignedShort() {
+		return readShort();
+	}
+	unsigned short readUnsignedShort(unsigned int offset) {
+		return readShort(offset);
+	}
+
+	int readInt() {
+		int int_ = ((int)(unsigned char)vector_[position + 3] << 24) | ((int)(unsigned char)vector_[position + 2] << 16) | ((int)(unsigned char)vector_[position + 1] << 8) | (unsigned char)vector_[position];
+		if (position >= size -3) {
+			throw 1;
+		}
+		position += 4;
+		bytesAvailable_ = size - position;
+		return int_;
+	}
+	int readInt(unsigned int offset) {
+		if (offset < size - 3) {
+			position = offset;
+		}
+		return readInt();
+	}
+
+	unsigned int readUnsignedInt() {
+		return readInt();
+	}
+	unsigned int readUnsignedInt(unsigned int offset) {
+		return readInt(offset);
+	}
+
+private:
+	/* The vector of chars */
+	vector<char> vector_;
+
+	/* Position of the offset in read and write operations */
+	unsigned int position = 0;
+
+	/* Number of bytes available to read */
+	unsigned int bytesAvailable_;
+
+	/* Fixed size of the ByteArray */
+	unsigned int size = 0;
+};
---
+++ Main.cpp
@@ -0,0 +1,104 @@
+//============================================================================
+// Name        : Main.cpp
+// Author      : Thulio Ferraz Assis
+// Contact     : contato@thulioassis.com.br
+// Copyright   : Thulio Ferraz Assis all rights reserved
+//============================================================================
+
+#include <iostream>
+#include "ByteArray.h"
+
+using namespace std;
+
+void print(string str, int color, bool bold) {
+	cout << "\033[" << (bold ? "1;" : "") << color << "m" << str << "\033[0m";
+}
+void print(string str, int color) {
+	print(str, color, false);
+}
+void print(string str) {
+	print(str, 30, false);
+}
+
+int main() {
+	print(">> Testing ByteArray.h <<\n\n", 31, true);
+
+	ByteArray testArray1(11);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing write operations\n", 31, false);
+	print("> Testing writeByte(105)... ", 30, true);
+	print(testArray1.writeByte(105) ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing writeShort(31000)... ", 30, true);
+	print(testArray1.writeShort(31000) ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing unsigned writeShort(45000)... ", 30, true);
+	print(testArray1.writeShort(45000) ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing negative writeShort(-31000)... ", 30, true);
+	print(testArray1.writeShort(-31000) ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing writeInt(145845304)... ", 30, true);
+	print(testArray1.writeInt(145845304) ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Trying to exceed capacity with writeShort(310)... ", 30, true);
+	print(testArray1.writeShort(31000) ? "error!\n" : "passed! Short not written\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Switching position to 0... ", 30, true);
+	testArray1.setPosition(0);
+	print(testArray1.getPosition() == 0 ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+
+	print("\n> Testing read operations\n", 31, false);
+	print("> Testing readByte() expected 105... ", 30, true);
+	print(testArray1.readByte() == 105 ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing readShort() expected 31000... ", 30, true);
+	print(testArray1.readShort() == 31000 ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing readUnsignedShort() expected 45000... ", 30, true);
+	print(testArray1.readUnsignedShort() == 45000 ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing readShort() expected -31000... ", 30, true);
+	print(testArray1.readShort() == -31000 ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Testing readInt() expected 145845304... ", 30, true);
+	print(testArray1.readInt() == 145845304 ? "passed!\n" : "error!\n", 31);
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Trying to exceed read with readByte()... ", 30, true);
+	try {
+		testArray1.readByte();
+		print("error!\n", 31);
+	} catch(int e) {
+		print("passed!\n", 31);
+	}
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+	print("> Trying to exceed read with readShort() in position (size - 1)... ", 30, true);
+	try {
+		testArray1.setPosition(testArray1.getSize() - 1);
+		testArray1.readShort();
+		print("error!\n", 31);
+	} catch(int e) {
+		print("passed!\n", 31);
+		testArray1.setPosition(testArray1.getSize());
+	}
+	cout << "> Position: " << testArray1.getPosition() << " | Size: " << testArray1.getSize() << " | Bytes Available: " << testArray1.bytesAvailable() << endl;
+
+	print("\n> Testing creation of another ByteArray using a vector of char\n", 31, false);
+	char newchars[7] = {-16,2,77,120,20,52,12};
+	vector<char> vectornewchars(newchars, newchars + sizeof(newchars) / sizeof(char) );
+	ByteArray testArray2(vectornewchars);
+
+	print("\n> Testing read operations\n", 31, false);
+	print("> Testing readByte()... ", 30, true);
+	cout << int(testArray2.readByte()) << endl;
+	cout << "> Position: " << testArray2.getPosition() << " | Size: " << testArray2.getSize() << " | Bytes Available: " << testArray2.bytesAvailable() << endl;
+	print("> Testing readShort()... ", 30, true);
+	cout << int(testArray2.readShort()) << endl;
+	cout << "> Position: " << testArray2.getPosition() << " | Size: " << testArray2.getSize() << " | Bytes Available: " << testArray2.bytesAvailable() << endl;
+	print("> Testing readInt()... ", 30, true);
+	cout << int(testArray2.readInt()) << endl;
+	cout << "> Position: " << testArray2.getPosition() << " | Size: " << testArray2.getSize() << " | Bytes Available: " << testArray2.bytesAvailable() << endl;
+
+	return 0;
+}
